e7fc1fe7c7d6ad656ffeb7f5434206f3129d66b2
SMG161 <151572311+SMG161@users.noreply.github.com>
2024-01-28 21:04:07 +0500

    backend

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cors());
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(`listening on port ${process.env.PORT}`);
+      console.log("Connected to Database");
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
