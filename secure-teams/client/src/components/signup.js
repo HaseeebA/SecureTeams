bd782124ee8808c6b901c18fbe152b9c8aca8d2e
as1725 <69496876+as1725@users.noreply.github.com>
2024-02-07 20:50:24 +0500

    singup+login implementated at backend

--- secure-teams/client/src/components/signup.js
+++ secure-teams/client/src/components/signup.js
@@ -1,49 +1,120 @@
 import React, { useState } from "react";
+import StarsCanvas from "./canvas/Stars";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 import "../styles/signup.css";
-import StarsCanvas from './canvas/Stars';
 
-const Signup = () => {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+const Signup = (props) => {
+	const [name, setName] = useState("");
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [role, setRole] = useState("");
 
-  const handleSignup = (e) => {
-    e.preventDefault();
-    // Perform signup logic here, e.g., send data to the server
-    console.log("Signup data:", { name, email, password });
-  };
+	const handleSignup = async (event) => {
+		event.preventDefault();
+		// const navigate = useNavigate();
 
-  return (
-    <div className="signup-container">
-      <StarsCanvas />
-      <form className="signup-form">
-        <input
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          placeholder="Name"
-          type="text"
-          className="signup-input"
-        />
-        <input
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="Email"
-          type="email"
-          className="signup-input"
-        />
-        <input
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          placeholder="Password"
+    const role = document.querySelector("select").value;
+
+		console.log("Name: " + name);
+		console.log("Password: " + password);
+    console.log("Email: " + email);
+		console.log("Role: " + role);
+
+		if (name === "") {
+			alert("Name cannot be empty");
+			return;
+		}
+
+		if (password !== confirmPassword) {
+			alert("Passwords do not match!");
+			return;
+		}
+
+		if (password.length < 8) {
+			alert("Password must be at least 8 characters long");
+			return;
+		}
+
+		if (!/\d/.test(password)) {
+			alert("Password must contain at least one number");
+			return;
+		}
+
+		if (!/[!@#$%^&*.]/.test(password)) {
+			alert("Password must contain at least one special character");
+			return;
+		}
+
+		try {
+			const response = await axios.post("http://localhost:3000/api/signup", {
+				name: name,
+        email: email,
+        password: password,
+        role: role,
+			});
+			console.log(response.data);
+      setName("");
+      setEmail("");
+      setPassword("");
+
+      alert("Signup successful! Redirecting to the login page...");
+      // navigate("/login");
+      window.location = "/login";
+		} catch (error) {
+			alert("Error signing up");
+			console.log(error);
+		}
+
+		// Alternatively, you can use the window.location object to redirect to the login page
+		// window.location = "/login";
+	};
+
+	return (
+		<div className="signup-container">
+			<StarsCanvas />
+			<form className="signup-form">
+				<input
+					value={name}
+					onChange={(e) => setName(e.target.value)}
+					placeholder="Name"
+					type="text"
+					className="signup-input"
+				/>
+				<input
+					value={email}
+					onChange={(e) => setEmail(e.target.value)}
+					placeholder="Email"
+					type="email"
+					className="signup-input"
+				/>
+				<input
+					value={password}
+					onChange={(e) => setPassword(e.target.value)}
+					placeholder="Password"
+					type="password"
+					className="signup-input"
+				/>
+				<input
+					value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+          placeholder="Confirm Password"
           type="password"
           className="signup-input"
-        />
-        <button type="submit" onClick={handleSignup} className="signup-button">
-          Sign Up
-        </button>
-      </form>
-    </div>
-  );
+				/>
+				<select name="role" className="signup-input">
+					<option value="team-member">Team Member</option>
+          <option value="team-lead">Team Lead</option>
+          <option value="manager">Manager</option>
+          <option value="admin">Admin</option>
+				</select>
+				<button type="submit" onClick={handleSignup} className="signup-button">
+					Sign Up
+				</button>
+			</form>
+		</div>
+	);
 };
 
 export default Signup;
--- secure-teams/server/server.js
+++ secure-teams/server/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 dotenv.config();
 
@@ -12,24 +14,73 @@ app.use(express.json());
 app.use(cors());
 
 mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => {
-    app.listen(process.env.PORT, () => {
-      console.log(`listening on port ${process.env.PORT}`);
-      console.log("Connected to Database");
-    });
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+	.connect(process.env.MONGO_URI)
+	.then(() => {
+		app.listen(process.env.PORT, () => {
+			console.log(`listening on port ${process.env.PORT}`);
+			console.log("Connected to Database");
+		});
+	})
+	.catch((error) => {
+		console.log(error);
+	});
 
 const userSchema = new mongoose.Schema({
-  username: String,
-  password: String,
-  email: String,
-  role: String,
-  teams: [String],
+	name: { type: String, required: true },
+	password: { type: String, required: true },
+	email: { type: String, required: true, unique: true },
+	role: {
+		type: String,
+		required: true,
+		enum: ["team-member", "team-lead", "manager", "admin"],
+	},
+	teams: { type: Array },
 });
 
 const User = mongoose.model("User", userSchema);
 
+app.post("/api/signup", async (req, res) => {
+	const { name, email, password, role } = req.body;
+	try {
+		let user = await User.findOne({ email: email });
+		if (user) {
+			console.log("User already exists");
+			return res.status(400).json({ message: "User already exists" });
+		}
+
+		const salt = await bcrypt.genSalt(10);
+		const hashedPassword = await bcrypt.hash(password, salt);
+		user = new User({ name, email, password: hashedPassword, role });
+		await user.save();
+		res.status(201).json({ message: "User created successfully" });
+		console.log("User created successfully");
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({ message: "Error creating user" });
+	}
+});
+
+app.post("/api/login", async (req, res) => {
+	const { email, password } = req.body;
+	try {
+		const user = await User.findOne({ email: email });
+		if (!user) {
+			console.log("User not found");
+			return res.status(400).json({ message: "User not found" });
+		}
+		const validPassword = await bcrypt.compare(password, user.password);
+		if (!validPassword) {
+			console.log("Invalid password");
+			return res.status(400).json({ message: "Invalid password" });
+		}
+		const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
+			expiresIn: "1h",
+		});
+    
+		res.status(200).json({ token: token });
+		console.log("Login successful");
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({ message: "Error logging in" });
+	}
+});
