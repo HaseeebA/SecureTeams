c13046cd05bae083fc580bdda5724f85a8a89fc9
Momin-star-beepboop <momin.kashif81@gmail.com>
2024-02-26 21:53:11 +0500

    fixed the issue of theme color not changing on the homepage

--- secure-teams/client/src/components/homepage.jsx
+++ secure-teams/client/src/components/homepage.jsx
@@ -5,7 +5,8 @@ import Sidepanel from './sidepanel';
 
 const Homepage = () => {
     const [showSidePanel, setShowSidePanel] = useState(true);
-    const initialTheme = localStorage.getItem('themeColor');
+    const initialTheme = localStorage.getItem('themeColor') || '#68d391';
+    console.log('initial theme:', initialTheme)
     const [theme, setTheme] = useState(initialTheme);
 
     const handleThemeChange = (newTheme) => {
