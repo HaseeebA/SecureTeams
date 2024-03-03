9b6234b3216b3c94ac137d2d47ab2e5430aad9c6
Momin-star-beepboop <momin.kashif81@gmail.com>
2024-03-03 15:21:37 +0500

    done show hide password

--- secure-teams/client/src/components/login.js
+++ secure-teams/client/src/components/login.js
@@ -3,10 +3,13 @@ import "../styles/login.css";
 import "./homepage.jsx";
 import StarsCanvas from "./canvas/Stars.jsx";
 import axios from "axios";
+import { Link } from "react-router-dom";
+import { closed, open } from "../images/index.js";
 
 const Login = () => {
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
+	const [showPassword, setShowPassword] = useState(false);
 
 	const handleLogin = async (event) => {
 		event.preventDefault();
@@ -45,6 +48,10 @@ const Login = () => {
 		}
 	};
 
+	const togglePasswordVisibility = () => {
+		setShowPassword(!showPassword);
+	};
+
 	return (
 		<div className="login-bg">
 			<div className="login">
@@ -58,15 +65,28 @@ const Login = () => {
 						type="email"
 						className="login-input"
 					/>
-					<input
-						value={password}
-						onChange={(e) => setPassword(e.target.value)}
-						placeholder="Password"
-						type="password"
-						className="login-input"
-					/>
+					<div className="input-container">
+						<input
+							type={showPassword ? "text" : "password"}
+							value={password}
+							onChange={(e) => setPassword(e.target.value)}
+							placeholder="Password"
+							className="password-input"
+						/>
+						<img
+							src={showPassword ? open : closed}
+							alt={showPassword ? "Hide password" : "Show password"}
+							className="toggle-password-visibility"
+							onClick={togglePasswordVisibility}
+						/>
+					</div>
 					<button type="submit" onClick={handleLogin} className="login-button">
 						Sign In
+					</button>
+					<button type="submit" className="login-button">
+						<Link to="/signup">
+							Sign Up
+						</Link>
 					</button>
 				</form>
 			</div>
