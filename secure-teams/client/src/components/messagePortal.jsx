4891363a1e9e046f20fcd07ecb5eea321f5cc09d
HaseeebA <25100122@lums.edu.pk>
2024-04-12 20:14:16 +0500

    messages tweaked

--- secure-teams/client/src/components/messagePortal.jsx
+++ secure-teams/client/src/components/messagePortal.jsx
@@ -1,16 +1,20 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect , useRef} from 'react';
 import axios from 'axios';
 import '../styles/msgportal.css';
 
 const ContactDetailsComponent = ({ receiver, sender }) => {
     const [messages, setMessages] = useState([]);
-
+    const messageListRef = useRef(null);
     useEffect(() => {
         // Fetch messages when component mounts
         fetchMessages();
         const intervalId = setInterval(fetchMessages, 1500); // Fetch messages every 3 seconds
         return () => clearInterval(intervalId); // Cleanup function to clear the interval
     }, [receiver]); // Include receiver in the dependency array
+
+    useEffect(() => {
+        scrollToBottom();
+    }, [messages]); // Scroll to bottom whenever messages change
 
     const fetchMessages = async () => {
         try {
@@ -27,14 +31,10 @@ const ContactDetailsComponent = ({ receiver, sender }) => {
             // Determine which response to use based on status and set the messages state accordingly
             if (response1.status === 200 && response2.status === 200) {
                 setMessages(response1.data.concat(response2.data)); // Set messages state with fetched messages
-                console.log('response1:', response1.data);
-                console.log('response2:', response2.data);
             } else if (response1.status === 200) {
                 setMessages(response1.data); // Set messages state with fetched messages;
-                console.log('response1:', response1.data);
             } else if (response2.status === 200) {
                 setMessages(response2.data); // Set messages state with fetched messages
-                console.log('response2:', response2.data);
             } else {
                 console.error('Failed to fetch messages');
             }
@@ -43,26 +43,28 @@ const ContactDetailsComponent = ({ receiver, sender }) => {
         }
     };
 
+    const scrollToBottom = () => {
+        if (messageListRef.current) {
+            messageListRef.current.scrollTop = messageListRef.current.scrollHeight;
+        }
+    };
+
     return (
         <div>
             <div className="contact-info">
-            <h3 className=''>{receiver.split('@')[0]}</h3>
+                <h3 className=''>{receiver.split('@')[0]}</h3>
             </div>
-            <div className="">
-                <div className="">
-                    <div className="message-list">
-                        {messages
-                            .sort((a, b) => new Date(a.timestamp) - new Date(b.timestamp))
-                            .map((message, index) => (
-                                <div
-                                    key={index}
-                                    className={`message ${message.sender === receiver ? 'received' : 'sent'}`}
-                                >
-                                    <p>{message.message}</p>
-                                </div>
-                            ))}
-                    </div>
-                </div>
+            <div className="message-list">
+                {messages
+                    .sort((a, b) => new Date(a.timestamp) - new Date(b.timestamp))
+                    .map((message, index) => (
+                        <div
+                            key={index}
+                            className={`message ${message.sender === receiver ? 'received' : 'sent'}`}
+                        >
+                            <p>{message.message}</p>
+                        </div>
+                    ))}
             </div>
         </div>
     );
