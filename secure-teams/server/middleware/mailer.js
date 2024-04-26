ef88901e01f9df9d0766837364bb3a970c97a40b
as1725 <69496876+as1725@users.noreply.github.com>
2024-04-26 14:57:45 +0500

    slight changes

--- secure-teams/server/middleware/mailer.js
+++ secure-teams/server/middleware/mailer.js
@@ -4,11 +4,13 @@ dotenv.config();
 
 export const transporter = nodemailer.createTransport({
 	service: "gmail",
-	host: "smtp.gmail.com",
-	port: 465,
-	secure: true,
+	// host: "smtp.gmail.com",
+	// port: 465,
+	// secure: true,
 	auth: {
-		user: process.env.EMAIL_USERNAME,
-		pass: process.env.APP_PASSWORD,
+		// user: process.env.EMAIL_USERNAME,
+		user: "secureeteams@gmail.com",
+		// pass: process.env.APP_PASSWORD,
+		pass: "catl ckmp bybg ekap",
 	},
 });
